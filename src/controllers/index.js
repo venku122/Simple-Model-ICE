7954a3a1b1fe303b6e6724348d13fc25ef5dfb98
Cody Van De Mark <cavigm@rit.edu>
2016-10-17 11:18:23 -0400

    updated

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,45 +9,22 @@ const defaultData = {
   bedsOwned: 0,
 };
 
-// object for us to keep track of the last Cat we made and dynamically update it sometimes
-let lastAdded = new Cat(defaultData);
+let lastAdded;
 
 const hostIndex = (req, res) => {
-  res.render('index', {
-    currentName: lastAdded.name,
-    title: 'Home',
-    pageName: 'Home Page',
-  });
+
 };
 
 const readAllCats = (req, res, callback) => {
-  Cat.find(callback);
+
 };
 
 const readCat = (req, res) => {
-  const name1 = req.query.name;
-
-  const callback = (err, doc) => {
-    if (err) {
-      return res.json({ err });
-    }
-
-    return res.json(doc);
-  };
-
-  Cat.findByName(name1, callback);
+ 
 };
 
 const hostPage1 = (req, res) => {
-  const callback = (err, docs) => {
-    if (err) {
-      return res.json({ err }); // if error, return it
-    }
 
-    return res.render('page1', { cats: docs });
-  };
-
-  readAllCats(req, res, callback);
 };
 
 const hostPage2 = (req, res) => {
@@ -59,9 +36,25 @@ const hostPage3 = (req, res) => {
 };
 
 const getName = (req, res) => {
-  res.json({ name: lastAdded.name });
+
 };
 
+const setName = (req, res) => {
+  if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
+    return res.status(400).json({ error: 'firstname,lastname and beds are all required' });
+  }
+  
+};
+
+const searchName = (req, res) => {
+  if (!req.query.name) {
+    return res.json({ error: 'Name is required to perform a search' });
+  }
+};
+
+const updateLast = (req, res) => {
+	
+};
 
 const notFound = (req, res) => {
   res.status(404).render('notFound', {
@@ -75,5 +68,9 @@ module.exports = {
   page2: hostPage2,
   page3: hostPage3,
   readCat,
+  getName,
+  setName,
+  updateLast,
+  searchName,
   notFound,
 };
--- src/router.js
+++ src/router.js
@@ -4,10 +4,16 @@ const router = function (app) {
   app.get('/page1', controllers.page1);
   app.get('/page2', controllers.page2);
   app.get('/page3', controllers.page3);
+  app.get('/getName', controllers.getName);
+  app.get('/findByName', controllers.searchName);
 
   app.get('/', controllers.index);
 
   app.get('/*', controllers.notFound);
+
+  app.post('/setName', controllers.setName);
+
+  app.post('/updateLast', controllers.updateLast);
 };
 
 // export the router function
